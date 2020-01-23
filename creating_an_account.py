f61e5c8b9f967ff725711727d78463cbe262bcbe
Aaron Anish <55660675+aaronanish12@users.noreply.github.com>
2020-01-23 14:24:35 +0545

    Add files via upload

--- creating_an_account.py
+++ creating_an_account.py
@@ -4,42 +4,53 @@ option=int(input('>>'))
 if option==1:
     usr_name=input("User Name:")
     try:
-        if input('Password')==(open(usr_name,'r').readline()):
+        file1=open(usr_name,'r')
+        file1.seek(0)
+        pwd=file1.readline()
+        if input('Password')==pwd[0:len(pwd)-1]:
             pass
         else:
-            print('Incorrect Password')
+           print('Incorrect Password')
     except:
         print('User Name don\'t exit.' )
 if option==2:
     user_name= input('User Name:')
     file = open(user_name, 'w+')
     password = input('Password:')
-
     name=input('First Name:')
     name1=input('Middle Name:')
     name2=input('Last Name:')
-
     address=input('Address:')
-
     phone_number=input('Phone Number')
-
     id=input('Identification Number')
-
     email=input('Email address:')
-    if file.readline(1)!=email:
-        file.writelines(password+'\n')
-        file.writelines(email+'\n')
-        file.writelines(name + ' ' + name1 + ' ' + name2+'\n')
-        file.writelines(address+'\n')
-        file.writelines(phone_number+"\n")
-        file.writelines(id+'\n')
+    file.seek(1)
+    if file.readline(-1)!=email:
+        file.writelines(password)
+        file.writelines('\n')
+        file.writelines(email)
+        file.writelines('\n')
+        file.writelines(name + ' ' + name1 + ' ' + name2)
+        file.writelines('\n')
+        file.writelines(address)
+        file.writelines('\n')
+        file.writelines(phone_number)
+        file.writelines('\n')
+        file.writelines(id)
+        file.writelines('\n')
 
 if option==3:
-    input('First Name')
-    input('Middle Name')
-    input('Last Name')
-    input('Country')
-    input('Contact Information')
+    name=input('First Name')
+    name1=input('Middle Name')
+    name2=input('Last Name')
+    country=input('Country')
+    file2 = open("guest_" + name+'_'+country, 'w+')
+    contact=input('Contact Information')
+    file2.writelines(name+' '+name1+' '+name2)
+    file2.writelines("\n")
+    file2.writelines(country)
+    file2.writelines('\n')
+    file2.writelines(contact)
 
 
 
